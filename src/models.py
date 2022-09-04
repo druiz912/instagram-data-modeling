240aac39cd89b8d49578339101c7b36624f30a1e
druiz912 <druiz264@gmail.com>
2022-09-04 19:51:41 +0000

    fix the exercise, starwars-data-model

--- src/models.py
+++ src/models.py
@@ -7,51 +7,40 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-class Usuario(Base):
-    __tablename__ = 'usuario'
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(20), unique=True, nullable=False)
-    email = Column(String(30), unique=True, nullable=False)
-    password= Column(Integer, unique=False, nullable=False)
-    posts = relationship('post', backref='usuario', lazy=True)
-    friends = relationship('friend', backref='usuario', lazy=True)
-    followers = relationship('followers', backref='usuario', lazy=True)
-    my_saved= relationship('saved', backref='usuario', lazy=True)
+class Users(Base):
+    __tablename__ = 'users'
+    id_user = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    last_name = Column(String(30), nullable=False)
+    email = Column(String(30), nullable=False)
+    password = Column(String(12), nullable=False)
+    favourites = relationship("Favourites", backref= "users", lazy=True)
 
-class Post(Base):
-    __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(100),unique=False, nullable=False)
-    text = Column(String(300),unique=False, nullable=False)
-    location = Column(String(100), unique=False, nullable=True)
-    likes = Column(Integer, nullable=False)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    saved_post = relationship('saved', backref='post',lazy=True)
+class Planets(Base):
+    __tablename__ = 'planets'
+    id_planets = Column(Integer, primary_key=True)
+    name_planets = Column(String(30))
+    coordinates = Column(Integer, nullable=True)
 
-class Friend(Base):
-    __tablename__ = 'friends'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    friend_id = Column(Integer, ForeignKey('usuario.id'))
+class Characters(Base):
+    __tablename__ = 'characters'
+    id_characters = Column(Integer, primary_key=True)
+    name_characters = Column(String(30))
+    breed = Column(String(30))
 
-class BestFriends(Base):
-    __tablename__="bestfriends"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    friend_id = Column(Integer,ForeignKey('usuario.id'))
+class Starships(Base):
+    __tablename__ = 'starships'
+    id_starship = Column(Integer, primary_key=True)
+    name_starship = Column(String(30))
+    origin = Column(String(50))
 
-class Follower(Base):
-    __tablename__ = 'followers'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    follower_id = Column(Integer, ForeignKey('usuario.id'))
-
-
-class Saved(Base):
-    __tablename__ = 'saved'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
+class Favourites(Base):
+    __tablename__ = 'favorites'
+    id_favourite = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('users.id_user'), nullable=False)
+    id_planets = Column(Integer, ForeignKey('planets.id_planet'))
+    id_characters = Column(Integer, ForeignKey('characters.id_character'))
+    id_starship = Column(Integer, ForeignKey('starships.id_starship'))    
 
     def to_dict(self):
         return {}
@@ -61,5 +50,5 @@ try:
     result = render_er(Base, 'diagrama.png')
     print("Bien! diagrama.png generado")
 except Exception as e:
-    print("Comprueba el código algún error hay")
+    print("Comprueba el código algún error hay...")
     raise e
